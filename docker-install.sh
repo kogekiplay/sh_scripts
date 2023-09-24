18c29dd6b2701b310e2d4c65979b65ae46d77883
Kogeki <46434871+kogekiplay@users.noreply.github.com>
2023-09-24 15:10:39 +0800

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -30,4 +30,4 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 # rm install.sh
-rm install.sh
+rm docker-install.sh
